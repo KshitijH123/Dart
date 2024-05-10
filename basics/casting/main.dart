86b94aaf7673682e7c3b6ac60dde50e0cd4f2b2a
Kshitij <kshitijhapase@gmail.com>
2024-05-10 20:49:36 +0530

    commit

---
+++ basics/casting/main.dart
@@ -0,0 +1,26 @@
+//Type Casting
+// int to double
+// double to int
+// string to int
+// string to double
+// int to string 
+// int to double
+
+
+void main() {
+
+  // int to double
+  int num1 =10;
+  double num2 = num1.toDouble();
+
+  // double to int
+  int num3 = num2.round();
+
+  // string to int
+  String num4 = '10';
+  int num5 = int.parse(num4);
+
+  
+
+
+}
--- classes/animal/animal.dart
+++ classes/animal/animal.dart
@@ -1,33 +1,17 @@
-class Animal{
+class Animal {
   String name;
   int height;
   int weight;
 
-  Animal(this.name, this.height, this.weight){
+  Animal({
+   required this.name,
+   required this.height,
+   required this.weight,
+  }) {
     print('Constructor called for $name');
   }
 
-  void run(){
+  void run() {
     print('$name is Running');
   }
-
-}
-
-void main(){
-  final cat = Animal('Cat', 120, 5);
-  final dog = Animal('Dog', 150, 15);
-
-  // print('Animal Name : ${cat.name}');
-  // print('Animal Height : ${cat.height}');
-  // print('Animal Weight : ${cat.weight}\n');
-
-  // print('Animal Name : ${dog.name}');
-  // print('Animal Height : ${dog.height}');
-  // print('Animal Weight : ${dog.weight}');
-
-
-  cat.run();
-  dog.run();
-
-
 }
